6cf1a94f8f64d35a665545a31e079fed22b7a916
Ramon Llorente <ramonbllorente@gmail.com>
2021-03-08 14:11:59 -0300

    fix process

--- meli_pull_apidata.py
+++ meli_pull_apidata.py
@@ -64,10 +64,11 @@ class MeliApiClient:
             yield self.ITEMSMETRICS_URL.format(**params)
 
     def user_metrics_url_query_gen(self, user_ids):
-        for user in users:
+        for user in user_ids:
             params = {'user_id': user,
                       'date_from': self.DATE_FROM,
                       'date_to': self.DATE_TO}
+            yield self.USERMETRICS_URL.format(**params)
 
     def _batch_generator(self, iterable, size):
         sourceiter = iter(iterable)
@@ -94,25 +95,29 @@ class MeliApiClient:
         for ri in raw_data:
             for request in ri:
                 if request.ok:
-                    data = request.json().get(key)
-    #            if data:
-                    for i in range(len(data)):
-                        try:
-                            count += 1
-                            if key == 'results':
+                    if key == 'results':
+                        data = request.json().get(key)
+                    if key == 'results':
+                        for i in range(len(data)):
+                            try:
+                                count += 1
                                 master_dict[data[i].get('id')] = data[i]
-                            elif key == 'user_id':
-                                master_dict[request.json().get('user_id')] = data
-                            elif key == 'item_id':
-                                master_dict[request.json().get('item_id')] = data
-                            else:
-                                master_dict[request.json().get('id')] = data
-                        except IndexError as e:                            
-                            logging.warning(f"{e} in {i}")
-                        except KeyError as e:
-                            logging.warning(f"{e} in {i}")
-                        except AttributeError as e:
-                            logging.warning(f"{e} in {i}")
+                            except IndexError as e:                            
+                                logging.warning(f"{e} in {i}")
+                            except KeyError as e:
+                                logging.warning(f"{e} in {i}")
+                            except AttributeError as e:
+                                logging.warning(f"{e} in {i}")
+                    else:
+                        if key == 'id':
+                            count += 1
+                            master_dict[request.json().get(key)] = request.json()
+                        elif key == 'item_id':
+                            count += 1
+                            master_dict[request.json()[0].get(key)] = request.json()[0]
+                        elif key == 'user_id':
+                            count += 1
+                            master_dict[request.json().get(key)] = request.json()
         return master_dict, count
     
 if __name__ == '__main__':
@@ -122,48 +127,73 @@ if __name__ == '__main__':
        .split('.')[0])
     logging.basicConfig(filename=f'logs/api_meli_{now}.log', 
                     level=logging.INFO)
+
+    sites_file = 'results/meli_sites_api_data.p'
+    items_file = 'results/meli_items_api_data.p'
+    items_metrics_file = 'results/meli_items_metrics_api_data.p'
+    sellers_metrics_file = 'results/meli_users_metrics_api_data.p'
+
     # Creo instancia
     mac = MeliApiClient()
     # request de url sites
-    r = mac.thread_wrapper_requests(50, mac.sites_url_query_gen)
-    sites_dict, count_sites = mac.process_todict(r, 'results')
-    logging.info(f"{count_sites} results")
-    # guardo resultados en un diccionario
-    with open(f'results/meli_sites_api_data_{now}.p', 'wb') as f:
-        pickle.dump(sites_dict, f)
-    logging.info("Finished sites request")
+    if os.path.exists(sites_file):
+        with open(sites_file, 'rb') as f:
+            sites_dict = pickle.load(f)
+    else:
+        r = mac.thread_wrapper_requests(50, mac.sites_url_query_gen)
+        sites_dict, count_sites = mac.process_todict(r, 'results')
+        logging.info(f"{count_sites} results")
+        # guardo resultados en un diccionario
+        with open(sites_file, 'wb') as f:
+            pickle.dump(sites_dict, f)
+        logging.info("Finished sites request")
     
     # items stage
-    r2 = mac.thread_wrapper_requests(
-            50,
-            partial(mac.items_url_query_gen, sites_dict)
-            )
-    items_dict, count_items = mac.process_todict(r2, 'id')
-    logging.info(f"{count_items} results")
-    # guardo resultados en un diccionario
-    with open(f'results/meli_items_api_data_{now}.p', 'wb') as f:
-        pickle.dump(items_dict, f)
-    logging.info("Finished items request")
+    if os.path.exists(items_file):
+        with open(items_file, 'rb') as f:
+            items_dict = pickle.load(f)
+    else:
+        r2 = mac.thread_wrapper_requests(
+                50,
+                partial(mac.items_url_query_gen, sites_dict)
+                )
+        items_dict, count_items = mac.process_todict(r2, 'id')
+        logging.info(f"{count_items} results")
+        # guardo resultados en un diccionario
+        with open(items_file, 'wb') as f:
+            pickle.dump(items_dict, f)
+        logging.info("Finished items request")
 
     # items metrics
-    r3 = mac.thread_wrapper_requests(
-            50,
-            partial(mac.items_metrics_url_query_gen, sites_dict)
-            )
-    items_metrics_dict, count_item_metrics = mac.process_todict(r3, 'item_id')
-    logging.info(f"{count_item_metrics} results")
-    with open(f'results/meli_items_metrics_api_data_{now}.p', 'wb') as f:
-        pickle.dump(items_metrics_dict, f)
-    logging.info("Finished item metrics request")
+    print('items metrics')
+    if os.path.exists(items_metrics_file):
+        with open(items_metrics_file, 'rb') as f:
+            items_metrics_dict = pickle.load(f)
+    else:
+        r3 = mac.thread_wrapper_requests(
+                50,
+                partial(mac.items_metrics_url_query_gen, sites_dict)
+                )
+        items_metrics_dict, count_item_metrics = mac.process_todict(r3, 'item_id')
+        logging.info(f"{count_item_metrics} results")
+        with open(items_metrics_file, 'wb') as f:
+            pickle.dump(items_metrics_dict, f)
+        logging.info("Finished item metrics request")
 
     # user metrics
-    seller_ids = (item[k].get('seller_id') for k in items_metrics_dict)
-    r4 = mac.thread_wrapper_requests(
-            50,
-            partial(mac.user_metrics_url_query_gen,
-                    seller_ids))
-    seller_metrics_dict, count_seller_metrics = mac.process_todict(r4, 'user_id')
-    with open(f'results/meli_seller_metrics_api_data_{now}.p', 'wb') as f:
-        pickle.dump(seller_metrics_dict, f)
-    logging.info("Finished seller metrics request")
+    print('users metrics')
+    if os.path.exists(sellers_metrics_file):
+        with open(sellers_metrics_file, 'rb') as f:
+            seller_metrics_dict = pickle.load(f)
+    else:
+        seller_ids = (items_dict[k].get('seller_id') for k in items_dict)
+        r4 = mac.thread_wrapper_requests(
+                50,
+                partial(mac.user_metrics_url_query_gen,
+                        seller_ids))
+        sellers_metrics_dict, count_sellers_metrics = mac.process_todict(r4, 'user_id')
+        with open(sellers_metrics_file, 'wb') as f:
+            pickle.dump(sellers_metrics_dict, f)
+        logging.info("Finished seller metrics request")
+
 
